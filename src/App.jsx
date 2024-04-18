964598baa5b48a832ef7ec4d128949f3885d24f6
rockey223 <rockeym50@gmail.com>
2024-04-18 11:31:59 +0545

    build

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,45 @@
 import "./App.css";
-import { useSelector,useDispatch } from "react-redux";
-import { incNum,decNum,getData } from "./actions";
+import { useSelector, useDispatch } from "react-redux";
+import { incNum, decNum, getData } from "./actions";
 function App() {
-
-  const numberState = useSelector((state)=>{return state.changeNumber});
+  const numberState = useSelector((state) => {
+    return state.changeNumber;
+  });
   const dispatch = useDispatch();
-const Data = useSelector((state)=>{return state.allData.data})
+  const Data = useSelector((state) => {
+    return state.allData.data;
+  });
 
-function random (){
-  let x = Math.floor((Math.random() * 100) + 1);
-  return x
-
-}
+  function random() {
+    let x = Math.floor(Math.random() * 100 + 1);
+    return x;
+  }
   return (
     <>
       <h1>This is Redux</h1>
 
       <h2>Increase and decrease example</h2>
       <div className="displayquantity">
-        <button type="button" onClick={()=>dispatch(decNum())}>decrease</button>
-        <input type="text" name="number" id="number" value={numberState.number} />
-        
-        <button type="button" onClick={()=>dispatch(incNum())}>increase</button>
+        <button type="button" onClick={() => dispatch(decNum())}>
+          decrease
+        </button>
+        <input
+          type="text"
+          name="number"
+          id="number"
+          value={numberState.number}
+        />
+
+        <button type="button" onClick={() => dispatch(incNum())}>
+          increase
+        </button>
       </div>
-      <button type="button" onClick={()=>dispatch(getData(random()))}>Click me</button>
+      <button type="button" onClick={() => dispatch(getData(random()))}>
+        Click me
+      </button>
       <ul>
-{
-  Data.joke
-}
-      {/* {
+        {Data.joke}
+        {/* {
         Data.map((data,index)=>{
           
           return(
